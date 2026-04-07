d3a1572eb8d635e33c0f6410bd1562f3498683c6
Anoop Kumar Sharma <aks09042008@gmail.com>
2026-04-08 00:20:14 +0530

    Add files via upload

---
+++ ADBG/adbg.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
+#include "core\thrmng.h"
+#include "core\atcptr.h"
+#include "core\hasher.h"
+#include "core\syscall.h"
+#include "core\handler.h"
+
+#include "api\dbgpresent.h"
+#include "api\rdbgpresent.h"
+#include "api\dbgobjhandle.h"
+#include "api\procdbgflag.h"
+#include "api\procdbgport.h"
+
+#include "asm\dbgbreak.h"
+#include "asm\int2d.h"
+#include "asm\int3.h"
+#include "asm\sckreg.h"
+#include "asm\prehop.h"
+#include "asm\popf.h"
+#include "asm\lbr_btf.h"
+
+#include "exceptions\raiseexc.h"
+#include "exceptions\hwbreakp2.h"
+#include "exceptions\pgexcbp.h"
+
+#include "flags\kerneldbg.h"
+#include "flags\ntglobalflag.h"
+#include "flags\prochpflag.h"
+#include "flags\prochpforceflag.h"
+#include "flags\duphnd.h"
+#include "flags\prntproc.h"
+#include "flags\ntldt.h"
+#include "flags\job.h"
+#include "flags\timing.h"
+#include "flags\window.h"
+
+#include "memory\hwbreakp.h"
+#include "memory\readstck.h"
+#include "memory\peb.h"
+#include "memory\vrtalloc.h"
+#include "memory\membreakp.h"
+#include "memory\dbgp.h"
+
+#include "object\clshandle.h"
+#include "object\dbgobj.h"
+#include "object\opnproc.h"
+#include "object\prothnd.h"
+#include "object\sysdbgctl.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    typedef struct {
+        bool result;
+        const char* functionName;
+        union {
+            bool (*functionPtr)();
+            bool (*functionPtrWithProcess)(HANDLE);
+            bool (*functionPtrWithThread)(HANDLE);
+            bool (*functionPtrWithProcessAndThread)(HANDLE, HANDLE);
+        };
+    } DebugCheckResult;
+
+    extern DebugCheckResult debuggerChecks[];
+
+    void StartDebugProtection(); // guard mode
+    bool isProgramBeingDebugged(); // single run mode
+
+#ifdef __cplusplus
+}
+#endif
---
+++ ADBG/core/thrmng.c
@@ -0,0 +1,40 @@
+#include "thrmng.h"
+#include "syscall.h"
+
+HANDLE DbgCreateThread(
+    const HANDLE hProcess,
+    const SIZE_T dwStackSize,
+    const LPTHREAD_START_ROUTINE lpStartAddress,
+    const LPVOID lpParameter,
+    const DWORD dwCreationFlags,
+    const LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList,
+    LPDWORD lpThreadId)
+{
+    HANDLE hThread = NULL;
+#pragma warning (disable : 4152)
+    const NTSTATUS statusCreate = DbgNtCreateThreadEx(
+        &hThread,
+        THREAD_ALL_ACCESS,
+        NULL,
+        hProcess,
+        lpStartAddress,
+        lpParameter,
+        dwCreationFlags,
+        0,
+        dwStackSize,
+        dwStackSize,
+        (PPS_ATTRIBUTE_LIST)lpAttributeList);
+#pragma warning (default : 4152)
+
+    if (statusCreate < 0) {
+        SetLastError(statusCreate);
+        return NULL;
+    }
+
+    DbgNtSetInformationThread(hThread, ThreadHideFromDebugger, NULL, 0);
+
+    if (lpThreadId)
+        *lpThreadId = GetThreadId(hThread);
+
+    return hThread;
+}
